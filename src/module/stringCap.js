9e4241c9bb48ec65f2d1204ac0762717cc8ec078
girma <girmatarekegn.gi@gmail.com>
2022-11-23 19:34:29 +0300

    Add updeted post function

--- src/module/stringCap.js
+++ src/module/stringCap.js
@@ -2,7 +2,11 @@ const capitalize = (str) => {
   const strNew = str.trim().split(/\s+/);
   const strOne = strNew[0];
   const strTwo = strNew[1];
-  return `${strOne.charAt(0).toUpperCase() + strOne.slice(1)} ${strTwo.charAt(0).toUpperCase()}${strTwo.slice(1)}`;
+  if (!strTwo) {
+    return `${strOne.charAt(0).toUpperCase() + strOne.slice(1)}`;
+  } else {
+    return `${strOne.charAt(0).toUpperCase() + strOne.slice(1)} ${strTwo.charAt(0).toUpperCase() + strTwo.slice(1)}`;
+  }
 };
 
 export default capitalize;
