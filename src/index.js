7588e131175367df83dd639ea2628a1eaac4d0ac
girma <girmatarekegn.gi@gmail.com>
2022-11-24 17:11:54 +0300

    Add stayle and sucsses message

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Scores from './module/StudeScore.js';
 import postData from './module/sendingData.js';
 import capitalize from './module/stringCap.js';
 import getDataScores from './module/getingData.js';
+import succesFull from './module/succesfull.js';
+
+const closeNew = document.querySelector('.successefull');
 
 const form = document.querySelector('.form-container');
 const refreshBtn = document.querySelector('.ref-btn');
@@ -14,6 +17,12 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const newData = new Scores(capitalize(nameValue.value), scoreValue.value);
   postData(newData);
+  succesFull();
+  const closeBtn = document.querySelector('#close');
+  closeNew.classList.add('suc-green');
+  closeBtn.addEventListener('click', () => {
+    closeNew.classList.add('closeTab');
+  });
   nameValue.value = '';
   scoreValue.value = '';
 });
@@ -21,3 +30,5 @@ form.addEventListener('submit', (event) => {
 refreshBtn.addEventListener('click', () => {
   getDataScores();
 });
+
+
